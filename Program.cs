bbf9d28a63334f0fa5c9ce3fb109b1270bd3cd07
Will Roberts <willroberts3@protonmail.com>
2025-07-31 23:37:15 -0400

    Renames project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using kestrel_blazor_maui.Components;
+using blazor_kestrel.Components;
 
 // ASP.NET Core project templates use Kestrel by default when not hosted with IIS. In the following
 // template-generated Program.cs, the WebApplication.CreateBuilder method calls UseKestrel internally:
@@ -9,10 +9,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see
-		// https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see
+	// https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -23,6 +23,6 @@ app.UseAntiforgery();
 
 // Configure Kestrel in Blazor context.
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+	.AddInteractiveServerRenderMode();
 
 app.Run();
