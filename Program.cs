8e9bfa95d88528604a64e3bf6166a11e6e1302af
Will Roberts <willroberts3@protonmail.com>
2025-07-31 23:34:44 -0400

    Adds Kestrel notes

--- Program.cs
+++ Program.cs
@@ -1,26 +1,27 @@
 using kestrel_blazor_maui.Components;
 
+// ASP.NET Core project templates use Kestrel by default when not hosted with IIS. In the following
+// template-generated Program.cs, the WebApplication.CreateBuilder method calls UseKestrel internally:
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see
+		// https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+// Configure Kestrel in non-Blazor context.
+//app.MapGet("/", () => "hello world");
+
+// Configure Kestrel in Blazor context.
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
